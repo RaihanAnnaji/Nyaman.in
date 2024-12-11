c1f10fe5c254a2a32dc4a755c434e0081118fbde
RaihanAnnaji <raihanannaji1990@gmail.com>
2024-12-11 22:44:42 +0700

    first init github

---
+++ resources/views/front/byKota.blade.php
@@ -0,0 +1,44 @@
+<style>
+  .title_kota {
+    /* background-color: rgb(223, 215, 215);
+    opacity: 0.7; */
+  }
+</style>
+<section class="ftco-section testimony-section">
+    <div class="row justify-content-center">
+
+      <div class="col-md-3 mb-4">
+        <a href="{{url('kost/?kampus=unsoed')}}">
+          <div class="card text-bg-dark shadow">
+            <img src="{{asset('assets/images/kampus/unsoed.jpg')}}" class="card-img" weight="100%" alt="...">
+            <div class="card-img-overlay position-absolute w-100 h-100" style="background: rgba(0, 0, 0, 0.3);">
+              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:white; font-weight:bold; font-size:20px">Universitas Jenderal Soedirman</h5>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-md-3 mb-4">
+        <a href="{{url('kost/?kampus=telkom')}}">
+          <div class="card shadow">
+            <img src="{{asset('assets/images/kampus/telkom.jpg')}}" class="card-img" weight="100%" alt="...">
+            <div class="card-img-overlay position-absolute w-100 h-100" style="background: rgba(0, 0, 0, 0.3);">
+              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:white; font-weight:bold; font-size:20px">Universitas Telkom Purwokerto</h5>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div class="col-md-3 mb-4">
+        <a href="{{url('kost/?kampus=ump')}}">
+          <div class="card shadow">
+            <img src="{{asset('assets/images/kampus/UMP.jpeg')}}" class="card-img" weight="100%" alt="...">
+            <div class="card-img-overlay position-absolute w-100 h-100" style="background: rgba(0, 0, 0, 0.3);">
+              <p class="title_kota" style="position: absolute; top: 50%; left: 50%;transform: translate(-50%, -50%); color:white; font-weight:bold; font-size:20px">Universitas Muhammadiyah Purwokerto</h5>
+            </div>
+          </div>
+        </a>
+      </div>
+      </div>
+    </div>
+</section>
